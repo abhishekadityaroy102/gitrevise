19ccabfc9747013826bf1d5685f9b8d5b2d358c5
ABHISHEK KUMAR <101967057+abhishekadityaroy102@users.noreply.github.com>
2024-07-10 15:03:21 +0530

    first commit on b1

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,4 @@
 const http=require("http");
 const server=http.createServer((req,res)=>{
-    return res.statusCode(200).send({
-        res:"HELLO FROM SERVER SIDE"
-    })
+    return res.statusCode(200).send("HELLO FROM SERVER SIDE")
 })
